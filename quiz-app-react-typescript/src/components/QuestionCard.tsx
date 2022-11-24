6b0f79e1ca54123270c7713cc0313528b0e9c084
dewan-mizanur911 <dewan.mizanur911@gmail.com>
2022-11-24 12:19:48 +0600

    quiz app created with react typescript

---
+++ quiz-app-react-typescript/src/components/QuestionCard.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer: any;
+  questionNr: number;
+  totalQuestions: number;
+};
+
+const QuestionCard = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}: Props) => {
+  return (
+    <div key={userAnswer}>
+      <p className="number">
+        Question : {questionNr} / {totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }}></p>
+      <div>
+        {answers.map((answer) => (
+          <div>
+            <button disabled={userAnswer} value={answer} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answer }} />
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
